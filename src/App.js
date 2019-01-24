7b6055d1c61736b55c16d7e5590f90d7af85a83d
Tommo123 <646961851@qq.com>
2019-01-24 21:55:09 +0800

    React项目第一次提交

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import { NavBar, Icon } from 'antd-mobile';
+import { Toast} from 'antd-mobile';
 import './App.css';
+
+function showToast() {
+  Toast.info('This is a toast tips !!!', 1);
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button onClick={showToast}></button>
       </div>
+
     );
   }
 }
